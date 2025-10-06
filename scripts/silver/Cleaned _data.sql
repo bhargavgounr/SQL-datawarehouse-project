6bde85077e56940298754af23b2e9777fd2e0634
Bhargavgonur <95363240+bhargavgounr@users.noreply.github.com>
2025-10-06 06:07:37 +0530

    Create Cleaned _data.sql
    Cleaned data from Bronze and Inserted into created tables in silver

---
+++ scripts/silver/Cleaned _data.sql
@@ -0,0 +1,76 @@
+===========================================================================================
+--Correction on all the datasets which are in bronze and inserting them into created tables
+===========================================================================================
+
+--Inserting Corrected data to silver.crm_cust_info
+INSERT INTO silver.crm_cust_info (
+			cst_id, 
+			cst_key, 
+			cst_firstname, 
+			cst_lastname, 
+			cst_marital_status, 
+			cst_gndr,
+			cst_create_date
+		)
+		SELECT
+cst_id,
+cst_key,
+TRIM(cst_firstname) as cst_firstname,
+TRIM(cst_lastname) as cst_lastname,
+CASE
+	WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+	WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+	ELSE 'n/a'
+			END AS cst_gndr,
+CASE
+	WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+	WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+	ELSE 'n/a'
+			END AS cst_marital_status,
+cst_create_date
+FROM(
+SELECT
+*,
+ROW_NUMBER() over(partition by cst_id order by cst_create_date DESC) AS row_num_rank
+from bronze.crm_cust_info
+) AS T
+where row_num_rank = 1 and cst_id is not NULL;
+
+--inserted sucsessfully crm_cust_info
+
+--Inserting Corrected Data to silver.crm_prd_info
+INSERT INTO silver.crm_prd_info (
+			prd_id,
+			cat_id,
+			prd_key,
+			prd_nm,
+			prd_cost,
+			prd_line,
+			prd_start_dt,
+			prd_end_dt
+		)
+		SELECT
+			prd_id,
+			REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id, -- Extract category ID
+			SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,        -- Extract product key
+			prd_nm,
+			ISNULL(prd_cost, 0) AS prd_cost,
+			CASE 
+				WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
+				WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+				WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+				WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
+				ELSE 'n/a'
+			END AS prd_line, -- Map product line codes to descriptive values
+			CAST(prd_start_dt AS DATE) AS prd_start_dt,
+			CAST(
+				LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 
+				AS DATE
+			) AS prd_end_dt -- Calculate end date as one day before the next start date
+		FROM bronze.crm_prd_info
+--inserted sucessfully to silver.crm_prd_info
+
+
+
+
+
